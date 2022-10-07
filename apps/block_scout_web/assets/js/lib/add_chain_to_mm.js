10840e19deb2c9ae048e52bc2828de9a9bf77f23
bayupratama15 <bolehjugaemail@gmail.com>
2022-10-07 16:13:03 +0700

    updated chain

--- apps/block_scout_web/assets/js/lib/add_chain_to_mm.js
+++ apps/block_scout_web/assets/js/lib/add_chain_to_mm.js
@@ -1,49 +1,56 @@
-import 'bootstrap'
+import "bootstrap";
 
-export async function addChainToMM ({ btn }) {
+export async function addChainToMM({ btn }) {
   try {
-    const chainIDFromWallet = await window.ethereum.request({ method: 'eth_chainId' })
-    const chainIDFromInstance = getChainIdHex()
+    const chainIDFromWallet = await window.ethereum.request({
+      method: "eth_chainId",
+    });
+    const chainIDFromInstance = getChainIdHex();
 
-    const coinName = document.getElementById('js-coin-name').value
-    const subNetwork = document.getElementById('js-subnetwork').value
-    const jsonRPC = document.getElementById('js-json-rpc').value
+    const coinName = document.getElementById("js-coin-name").value;
+    const subNetwork = document.getElementById("js-subnetwork").value;
+    const jsonRPC = document.getElementById("js-json-rpc").value;
 
-    const blockscoutURL = location.protocol + '//' + location.host + process.env.NETWORK_PATH
+    // const blockscoutURL = location.protocol + '//' + location.host + process.env.NETWORK_PATH
+    const blockscoutURL = "https://explorer.cryonetwork.net/";
     if (chainIDFromWallet !== chainIDFromInstance) {
       await window.ethereum.request({
-        method: 'wallet_addEthereumChain',
-        params: [{
-          chainId: chainIDFromInstance,
-          chainName: subNetwork,
-          nativeCurrency: {
-            name: coinName,
-            symbol: coinName,
-            decimals: 18
+        method: "wallet_addEthereumChain",
+        params: [
+          {
+            chainId: chainIDFromInstance,
+            chainName: subNetwork,
+            nativeCurrency: {
+              name: coinName,
+              symbol: coinName,
+              decimals: 18,
+            },
+            rpcUrls: [jsonRPC],
+            blockExplorerUrls: [blockscoutURL],
           },
-          rpcUrls: [jsonRPC],
-          blockExplorerUrls: [blockscoutURL]
-        }]
-      })
+        ],
+      });
     } else {
-      btn.tooltip('dispose')
-      btn.tooltip({
-        title: `You're already connected to ${subNetwork}`,
-        trigger: 'click',
-        placement: 'bottom'
-      }).tooltip('show')
+      btn.tooltip("dispose");
+      btn
+        .tooltip({
+          title: `You're already connected to ${subNetwork}`,
+          trigger: "click",
+          placement: "bottom",
+        })
+        .tooltip("show");
 
       setTimeout(() => {
-        btn.tooltip('dispose')
-      }, 3000)
+        btn.tooltip("dispose");
+      }, 3000);
     }
   } catch (error) {
-    console.error(error)
+    console.error(error);
   }
 }
 
-function getChainIdHex () {
-  const chainIDFromDOM = document.getElementById('js-chain-id').value
-  const chainIDFromInstance = parseInt(chainIDFromDOM)
-  return chainIDFromInstance && `0x${chainIDFromInstance.toString(16)}`
+function getChainIdHex() {
+  const chainIDFromDOM = document.getElementById("js-chain-id").value;
+  const chainIDFromInstance = parseInt(chainIDFromDOM);
+  return chainIDFromInstance && `0x${chainIDFromInstance.toString(16)}`;
 }
